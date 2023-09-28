3885af7ccbba878731232c864f21042fbb1fd96b
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-09-28 08:57:27 -0300

    #6 [DOCS]: SwaggerUI and OpenAPI 3 documentation for Address

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import routes from "./routes";
+import express from "express"
+import bodyParser from "body-parser"
+import routes from "./routes"
+import swaggerDocs from "./config/openAPISpec"
+import swaggerUi from "swagger-ui-express"
 
-const app = express();
+// Express configuration
+const app = express()
+app.use(express.json())
+app.use(bodyParser.json())
 
-app.use(express.json());
-app.use(bodyParser.json());
-app.use("/api", routes);
+// Swagger UI route
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocs, {
+        explorer: true,
+    })
+)
 
-console.log("Server running on port 3000");
+// API routes
+app.use("/api", routes)
 
-const server = app.listen(3000);
+// Server starting point
+const server = app.listen(3000)
+console.log("Server running on port 3000")
---
+++ src/config/openAPISpec.ts
@@ -0,0 +1,15 @@
+import swaggerJsdoc from "swagger-jsdoc"
+
+const options: swaggerJsdoc.Options = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "PICCE API",
+            version: "1.0.0",
+        },
+    },
+    apis: ["./src/routes/*.ts"],
+}
+
+const openAPISpec = swaggerJsdoc(options)
+export default openAPISpec
