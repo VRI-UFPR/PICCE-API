13050e6301b3d725ddfcc01e6fe4871f6f4e9037
denielzzz <daniellins@ufpr.br>
2023-09-28 22:52:26 -0300

    #8 [FEAT]: CRUD for institution and schema definition

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import addressRoutes from "./addressRoutes";
+import institutionRoutes from "./institutionRoutes";
 
 const router = express.Router();
 
 router.use("/address", addressRoutes);
+router.use("/institution", institutionRoutes);
 
 export default router;
