2ca506ac76fc9b777510a031d2388aa6b9dc48c4
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-10-14 20:19:57 -0300

    #15 [FIX]: Naming and access of saved files

--- src/services/multerUploader.ts
+++ src/services/multerUploader.ts
@@ -2,12 +2,11 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.basename("uploads"));
-    },
+    destination: path.basename("uploads"),
     filename: function (req, file, cb) {
+        const fileExt = path.extname(file.originalname);
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + "-" + uniqueSuffix);
+        cb(null, "PICCE-" + file.fieldname + "-" + uniqueSuffix + fileExt);
     },
 });
 
