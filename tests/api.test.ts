2d8bb4702a93556c00acd469c7c24f79c88867b4
Izalorran Bonaldi <iosbonaldi@gmail.com>
2023-11-04 06:25:03 -0300

    #37 [FEAT]: Integration tests for applicationAnswer

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -2,17 +2,156 @@ import request from 'supertest';
 import express from 'express';
 import routes from '../src/routes';
 import prismaClient from '../src/services/prismaClient';
-import { addresses, dbDefaults } from './constants';
+import { addresses, applicationAnswers, dbDefaults } from './constants';
 import { serialize } from 'object-to-formdata';
 
 const app = express();
 app.use('/api', routes);
+const authorization = { id: 0, token: '' };
 
-beforeAll(async () => {
-    await prismaClient.address.deleteMany();
+describe('Auth tests', () => {
+    it('Should sign in a user', async () => {
+        const expectedResponse = {
+            message: 'User signed in.',
+            data: {
+                id: expect.any(Number),
+                token: expect.any(String),
+            },
+        };
+
+        const response = await request(app).post('/api/auth/signIn').field('username', 'johndoe').field('hash', '123456');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(expectedResponse);
+
+        authorization.id = response.body.data.id as number;
+        authorization.token = response.body.data.token as string;
+    });
+});
+
+describe('ApplicationAnswer tests', () => {
+    beforeAll(async () => {
+        await prismaClient.applicationAnswer.deleteMany();
+    });
+
+    for (const applicationAnswer of applicationAnswers) {
+        it('Should create an ApplicationAnswer', async () => {
+            const expectedResponse = {
+                message: 'Application answer created.',
+                data: {
+                    ...dbDefaults,
+                    ...applicationAnswer.original,
+                    itemAnswerGroups: expect.any(Array),
+                },
+            };
+
+            const req = request(app)
+                .post('/api/applicationAnswer/createApplicationAnswer')
+                .set('Authorization', `Bearer ${authorization.token}`);
+
+            for (const [name, value] of serialize(applicationAnswer.original, { indices: true })) {
+                req.field(name, value.toString());
+            }
+
+            const response = await req;
+
+            console.log(response.body);
+
+            expect(response.status).toBe(201);
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it('Should update an ApplicationAnswer', async () => {
+            const expectedResponse = {
+                message: 'Application answer updated.',
+                data: {
+                    ...applicationAnswer.original,
+                    ...applicationAnswer.updated,
+                    ...dbDefaults,
+                    itemAnswerGroups: expect.any(Array),
+                },
+            };
+
+            const req = request(app)
+                .put(`/api/applicationAnswer/updateApplicationAnswer/${applicationAnswer.original.id}`)
+                .set('Authorization', `Bearer ${authorization.token}`);
+
+            for (const [name, value] of serialize(applicationAnswer.updated, { indices: true })) {
+                req.field(name, value.toString());
+            }
+
+            const response = await req;
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it('Should get all ApplicationAnswers', async () => {
+            const expectedResponse = {
+                message: 'All application answers found.',
+                data: [
+                    {
+                        ...applicationAnswer.original,
+                        ...applicationAnswer.updated,
+                        ...dbDefaults,
+                        itemAnswerGroups: expect.any(Array),
+                    },
+                ],
+            };
+
+            const response = await request(app)
+                .get('/api/applicationAnswer/getAllApplicationAnswers')
+                .set('Authorization', `Bearer ${authorization.token}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it('Should get an ApplicationAnswer by id', async () => {
+            const expectedResponse = {
+                message: 'Application answer found.',
+                data: {
+                    ...applicationAnswer.original,
+                    ...applicationAnswer.updated,
+                    ...dbDefaults,
+                    itemAnswerGroups: expect.any(Array),
+                },
+            };
+
+            const response = await request(app)
+                .get(`/api/applicationAnswer/getApplicationAnswer/${applicationAnswer.original.id}`)
+                .set('Authorization', `Bearer ${authorization.token}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(expectedResponse);
+        });
+
+        it('Should delete an ApplicationAnswer', async () => {
+            const expectedResponse = {
+                message: 'Application answer deleted.',
+                data: {
+                    ...applicationAnswer.original,
+                    ...applicationAnswer.updated,
+                    ...dbDefaults,
+                    itemAnswerGroups: undefined,
+                },
+            };
+
+            const response = await request(app)
+                .delete(`/api/applicationAnswer/deleteApplicationAnswer/${applicationAnswer.original.id}`)
+                .set('Authorization', `Bearer ${authorization.token}`);
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual(expectedResponse);
+        });
+    }
 });
 
 describe('Address tests', () => {
+    beforeAll(async () => {
+        await prismaClient.address.deleteMany();
+    });
+
     for (const address of addresses) {
         it('Should create an Address', async () => {
             const expectedResponse = {
