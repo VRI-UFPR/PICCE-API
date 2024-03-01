39db2dd2cecf673bdec695032780fa0edaa80a0e
Izalorran Bonaldi <iosbonaldi@gmail.com>
2024-02-29 22:15:19 -0300

    #60 [FIX]: Authentication, authorization checks, error handling and refactored responses in user endpoints

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,44 @@ import { Response, Request } from 'express';
 import { User, UserRole } from '@prisma/client';
 import * as yup from 'yup';
 import prismaClient from '../services/prismaClient';
+import errorFormatter from '../services/errorFormatter';
+
+const checkAuthorization = (user: User, id: number) => {
+    if (user.role !== UserRole.ADMIN && user.id !== id) {
+        throw new Error('This user is not authorized to perform this action');
+    }
+};
+
+const checkHierarchy = (user: User, role: UserRole) => {
+    const coordinatorRestrictions = user.role === UserRole.COORDINATOR && (role === UserRole.ADMIN || role === UserRole.COORDINATOR);
+    const publisherRestrictions =
+        user.role === UserRole.PUBLISHER &&
+        (role === UserRole.ADMIN || role === UserRole.COORDINATOR || role === UserRole.PUBLISHER || role === UserRole.APLICATOR);
+    const otherRestrictions = user.role === UserRole.APLICATOR || user.role === UserRole.USER;
+
+    if (coordinatorRestrictions || publisherRestrictions || otherRestrictions) {
+        throw new Error('This user is not authorized to perform this action');
+    }
+};
+
+const fieldsWithNesting = {
+    name: true,
+    username: true,
+    role: true,
+    institution: {
+        select: {
+            id: true,
+            name: true,
+        },
+    },
+    classrooms: {
+        select: {
+            id: true,
+        },
+    },
+    createdAt: true,
+    updateAt: true,
+};
 
 export const createUser = async (req: Request, res: Response) => {
     try {
@@ -19,7 +57,11 @@ export const createUser = async (req: Request, res: Response) => {
 
         const user = await createUserSchema.validate(req.body);
 
-        const createdUser: User = await prismaClient.user.create({
+        const curUser = req.user as User;
+
+        checkHierarchy(curUser, user.role);
+
+        const createdUser = await prismaClient.user.create({
             data: {
                 name: user.name,
                 username: user.username,
@@ -27,11 +69,12 @@ export const createUser = async (req: Request, res: Response) => {
                 role: user.role,
                 institutionId: user.institutionId,
             },
+            select: fieldsWithNesting,
         });
 
         res.status(201).json({ message: 'User created.', data: createdUser });
     } catch (error: any) {
-        res.status(400).json({ error: error });
+        res.status(400).json(errorFormatter(error));
     }
 };
 
@@ -53,7 +96,11 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
 
         const user = await updateUserSchema.validate(req.body);
 
-        const updatedUser: User = await prismaClient.user.update({
+        const curUser = req.user as User;
+
+        checkAuthorization(curUser, id);
+
+        const updatedUser = await prismaClient.user.update({
             where: {
                 id,
             },
@@ -67,21 +114,24 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
                     disconnect: user.classrooms?.map((id) => ({ id: id })),
                 },
             },
+            select: fieldsWithNesting,
         });
 
         res.status(200).json({ message: 'User updated.', data: updatedUser });
     } catch (error: any) {
-        res.status(400).json({ error: error });
+        res.status(400).json(errorFormatter(error));
     }
 };
 
 export const getAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
-        const users: User[] = await prismaClient.user.findMany({});
+        if ((req.user as User).role !== UserRole.ADMIN) throw new Error('This user is not authorized to perform this action');
+
+        const users = await prismaClient.user.findMany({ select: fieldsWithNesting });
 
         res.status(200).json({ message: 'All users found.', data: users });
     } catch (error: any) {
-        res.status(400).json({ error: error });
+        res.status(400).json(errorFormatter(error));
     }
 };
 
@@ -89,18 +139,20 @@ export const getUser = async (req: Request, res: Response): Promise<void> => {
     try {
         const id: number = parseInt(req.params.userId);
 
-        const user: User = await prismaClient.user.findUniqueOrThrow({
+        const curUser = req.user as User;
+
+        checkAuthorization(curUser, id);
+
+        const user = await prismaClient.user.findUniqueOrThrow({
             where: {
                 id,
             },
-            include: {
-                classrooms: true,
-            },
+            select: fieldsWithNesting,
         });
 
         res.status(200).json({ message: 'User found.', data: user });
     } catch (error: any) {
-        res.status(400).json({ error: error });
+        res.status(400).json(errorFormatter(error));
     }
 };
 
@@ -108,14 +160,21 @@ export const deleteUser = async (req: Request, res: Response): Promise<void> => 
     try {
         const id: number = parseInt(req.params.userId);
 
-        const deletedUser: User = await prismaClient.user.delete({
+        const curUser = req.user as User;
+
+        checkAuthorization(curUser, id);
+
+        const deletedUser = await prismaClient.user.delete({
             where: {
                 id,
+            },
+            select: {
+                id: true,
             },
         });
 
         res.status(200).json({ message: 'User deleted.', data: deletedUser });
     } catch (error: any) {
-        res.status(400).json({ error: error });
+        res.status(400).json(errorFormatter(error));
     }
 };
