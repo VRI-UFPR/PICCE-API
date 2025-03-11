2551f5fd6508db857d91833f31b43c1bb0a03ffd
Izalorran Bonaldi <iosbonaldi@gmail.com>
2025-03-11 12:04:41 -0300

    #145 [FIX]: Documentation and standardization of nomenclature in address controller

--- src/controllers/addressController.ts
+++ src/controllers/addressController.ts
@@ -9,179 +9,272 @@ of the GNU General Public License along with PICCE-API.  If not, see <https://ww
 */
 
 import { Response, Request } from 'express';
-import { Address, User, UserRole } from '@prisma/client';
+import { User, UserRole } from '@prisma/client';
 import * as yup from 'yup';
 import prismaClient from '../services/prismaClient';
 import errorFormatter from '../services/errorFormatter';
 
-const checkAuthorization = async (user: User, addressId: number | undefined, action: string) => {
-    if (user.role === UserRole.ADMIN) return;
+/**
+ * Checks if the user is authorized to perform a specific action on an address.
+ *
+ * @param requester - The user object containing requester user details.
+ * @param action - The action the user wants to perform (e.g., 'create', 'update', 'delete', 'getAll', 'get', 'getByState', 'getId').
+ *
+ * @throws Will throw an error if the user is not authorized to perform the action.
+ * @returns A promise that resolves if the user is authorized to perform the action.
+ */
+const checkAuthorization = async (requester: User, action: string) => {
+    // ADMINs can perform all actions
+    if (requester.role === UserRole.ADMIN) return;
 
     switch (action) {
         case 'create':
         case 'update':
-        case 'delete':
-            // Only ADMINs can perform create/update/delete operations on addresses
+        case 'delete': {
+            // No one can perform create/update/delete operations on addresses
             throw new Error('This user is not authorized to perform this action');
+        }
         case 'getAll':
         case 'get':
         case 'getByState':
-        case 'getId':
+        case 'getId': {
             // Everyone can perform get/getAll/getByState operations on addresses
             break;
+        }
     }
 };
 
+/**
+ * Creates a new address in the database.
+ *
+ * This function handles the creation of a new address, validating the body of the request and
+ * the user performing the action to then persist the object in the database using Prisma.
+ *
+ * @param req - The request object, containing the address data in the body and the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const createAddress = async (req: Request, res: Response) => {
     try {
         // Yup schemas
         const createAddressSchema = yup
             .object()
             .shape({
-                id: yup.number().min(1),
                 city: yup.string().min(1).required(),
                 state: yup.string().min(1).required(),
                 country: yup.string().min(1).required(),
             })
             .noUnknown();
         // Yup parsing/validation
-        const address = await createAddressSchema.validate(req.body, { stripUnknown: false });
-        // User from Passport-JWT
-        const user = req.user as User;
+        const addressData = await createAddressSchema.validate(req.body, { stripUnknown: false });
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to create an address
-        await checkAuthorization(user, undefined, 'create');
+        await checkAuthorization(requester, 'create');
         // Prisma operation
-        const createdAddress: Address = await prismaClient.address.create({
-            data: { id: address.id, city: address.city, state: address.state, country: address.country },
-        });
-
-        res.status(201).json({ message: 'Address created.', data: createdAddress });
+        const storedAddress = await prismaClient.address.create({ data: addressData });
+        // Express success response
+        res.status(201).json({ message: 'Address created.', data: storedAddress });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Updates an existing address in the database.
+ *
+ * This function handles the update of a existing address, validating the body of the request and
+ * the user performing the action to then persist the object in the database using Prisma.
+ *
+ * @param req - The request object, containing the address data in the body, the user object from Passport-JWT and the address ID in the params.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const updateAddress = async (req: Request, res: Response): Promise<void> => {
     try {
         // ID from params
-        const addressId: number = parseInt(req.params.addressId);
+        const addressId = parseInt(req.params.addressId);
         // Yup schemas
         const updateAddressSchema = yup
             .object()
             .shape({ city: yup.string().min(1), state: yup.string().min(1), country: yup.string().min(1) })
             .noUnknown();
         // Yup parsing/validation
-        const address = await updateAddressSchema.validate(req.body, { stripUnknown: false });
-        // User from Passport-JWT
-        const user = req.user as User;
+        const addressData = await updateAddressSchema.validate(req.body, { stripUnknown: false });
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to update an address
-        await checkAuthorization(user, addressId, 'update');
+        await checkAuthorization(requester, 'update');
         // Prisma operation
-        const updatedAddress: Address = await prismaClient.address.update({
-            where: { id: addressId },
-            data: { city: address.city, state: address.state, country: address.country },
-        });
-
-        res.status(200).json({ message: 'Address updated.', data: updatedAddress });
+        const storedAddress = await prismaClient.address.update({ where: { id: addressId }, data: addressData });
+        // Express success response
+        res.status(200).json({ message: 'Address updated.', data: storedAddress });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Gets all addresses from the database.
+ *
+ * This function handles the retrieval of all addresses in the database, validating the user
+ * performing the action to then retrieve all addresses using Prisma.
+ *
+ * @param req - The request object, containing the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const getAllAddresses = async (req: Request, res: Response): Promise<void> => {
     try {
-        // User from Passport-JWT
-        const user = req.user as User;
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to get all addresses
-        await checkAuthorization(user, undefined, 'getAll');
+        await checkAuthorization(requester, 'getAll');
         // Prisma operation
-        const addresses: Address[] = await prismaClient.address.findMany();
-
-        res.status(200).json({ message: 'All addresses found.', data: addresses });
+        const storedAddresses = await prismaClient.address.findMany();
+        // Express success response
+        res.status(200).json({ message: 'All addresses found.', data: storedAddresses });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Gets all addresses from the database by state.
+ *
+ * This function handles the retrieval of all addresses in the database by state, validating the user
+ * performing the action to then retrieve all addresses using Prisma.
+ *
+ * @param req - The request object, containing the search parameters in the body and the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const getAddressesByState = async (req: Request, res: Response): Promise<void> => {
     try {
         // Yup schemas
-        const getAddressesByStateSchema = yup
-            .object()
-            .shape({ state: yup.string().min(1).required(), country: yup.string().min(1).required() });
+        const searchParamsSchema = yup.object().shape({ state: yup.string().min(1).required(), country: yup.string().min(1).required() });
         // Yup parsing/validation
-        const searchParams = await getAddressesByStateSchema.validate(req.body, { stripUnknown: false });
-        // User from Passport-JWT
-        const user = req.user as User;
+        const searchParams = await searchParamsSchema.validate(req.body, { stripUnknown: false });
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to get addresses by state
-        await checkAuthorization(user, undefined, 'getByState');
+        await checkAuthorization(requester, 'getByState');
         // Prisma operation
-        const addresses = await prismaClient.address.findMany({
-            where: { state: searchParams.state, country: searchParams.country },
-            select: { id: true, city: true },
-        });
-
-        res.status(200).json({ message: 'Addresses found.', data: addresses });
+        const storedAddresses = await prismaClient.address.findMany({ where: searchParams, select: { id: true, city: true } });
+        // Express success response
+        res.status(200).json({ message: 'Addresses found.', data: storedAddresses });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Gets the ID of an address from the database by city, state and country.
+ *
+ * This function handles the retrieval of an address ID in the database by city, state and country, validating the user
+ * performing the action to then retrieve the address ID using Prisma.
+ *
+ * @param req - The request object, containing the search parameters in the body and the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const getAddressId = async (req: Request, res: Response): Promise<void> => {
     try {
         // Yup schemas
-        const getCityIdSchema = yup.object().shape({
+        const searchParamsSchema = yup.object().shape({
             city: yup.string().min(1).required(),
             state: yup.string().min(1).required(),
             country: yup.string().min(1).required(),
         });
         // Yup parsing/validation
-        const searchParams = await getCityIdSchema.validate(req.body, { stripUnknown: false });
-        // User from Passport-JWT
+        const searchParams = await searchParamsSchema.validate(req.body, { stripUnknown: false });
+        // Requester user from Passport-JWT
         const user = req.user as User;
         // Check if user is authorized to get a city ID
-        await checkAuthorization(user, undefined, 'getId');
+        await checkAuthorization(user, 'getId');
         // Prisma operation
-        const { id: cityId } = await prismaClient.address.findUniqueOrThrow({
-            where: { city_state_country: { city: searchParams.city, state: searchParams.state, country: searchParams.country } },
+        const storedAddress = await prismaClient.address.findUniqueOrThrow({
+            where: { city_state_country: searchParams },
             select: { id: true },
         });
-
-        res.status(200).json({ message: 'City ID found.', data: cityId });
+        // Express success response
+        res.status(200).json({ message: 'City ID found.', data: storedAddress.id });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Gets an address from the database by ID.
+ *
+ * This function handles the retrieval of an address in the database by ID, validating the user
+ * performing the action to then retrieve the address using Prisma.
+ *
+ * @param req - The request object, containing the address ID in the params and the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const getAddress = async (req: Request, res: Response): Promise<void> => {
     try {
         // ID from params
-        const addressId: number = parseInt(req.params.addressId);
-        // User from Passport-JWT
-        const user = req.user as User;
+        const addressId = parseInt(req.params.addressId);
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to get an address
-        await checkAuthorization(user, addressId, 'get');
+        await checkAuthorization(requester, 'get');
         // Prisma operation
-        const address: Address = await prismaClient.address.findUniqueOrThrow({ where: { id: addressId } });
-
-        res.status(200).json({ message: 'Address found.', data: address });
+        const storedAddress = await prismaClient.address.findUniqueOrThrow({ where: { id: addressId } });
+        // Express success response
+        res.status(200).json({ message: 'Address found.', data: storedAddress });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
 
+/**
+ * Deletes an address from the database by ID.
+ *
+ * This function handles the deletion of an address in the database by ID, validating the user
+ * performing the action to then delete the address using Prisma.
+ *
+ * @param req - The request object, containing the address ID in the params and the user object from Passport-JWT.
+ * @param res - The response object, used to send the response back to the client.
+ *
+ * @returns A promise that resolves when the function sets the response to the client.
+ */
 export const deleteAddress = async (req: Request, res: Response): Promise<void> => {
     try {
         // ID from params
-        const addressId: number = parseInt(req.params.addressId);
-        // User from Passport-JWT
-        const user = req.user as User;
+        const addressId = parseInt(req.params.addressId);
+        // Requester user from Passport-JWT
+        const requester = req.user as User;
         // Check if user is authorized to delete an address
-        await checkAuthorization(user, addressId, 'delete');
+        await checkAuthorization(requester, 'delete');
         // Prisma operation
         const deletedAddress = await prismaClient.address.delete({ where: { id: addressId }, select: { id: true } });
+        // Express success response
         res.status(200).json({ message: 'Address deleted.', data: deletedAddress });
     } catch (error: any) {
+        console.error(error);
+        // Express error response
         res.status(400).json(errorFormatter(error));
     }
 };
